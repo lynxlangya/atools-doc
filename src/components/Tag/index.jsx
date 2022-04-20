00cde4444f1f17223469bce2299d0b52c60bc59c
山人自有妙计 <wangdaoo@yeah.net>
2022-04-20 21:20:57 +0800

    feat: update version :rocket:

--- src/components/Tag/index.jsx
+++ src/components/Tag/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Image from '@theme/IdealImage';
 import styles from './styles.module.css';
-import Version from '/img/version.png';
 
-export const Tag = ({ children, color }) => (
+const Version = require('@site/static/svg/version.svg').default;
+
+export const Tag = ({ children }) => (
   <div className={styles.tagBox}>
-    <Image img={Version} style={{ width: '16px', marginRight: '5px' }} />
+    <Version className={styles.svg} />
     <span>{children}</span>
   </div>
 );
